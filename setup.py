e5af876485a6c6ec538c4f1c0553c28e0764f7e8
Matthew R. Scott <gldnspud@gmail.com>
2009-01-19 14:46:36 -0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='MulTweetTwitter',
+      version=version,
+      description="Twitter plugin for MulTweet",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='ElevenCraft, Inc.',
+      author_email='matt@11craft.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
